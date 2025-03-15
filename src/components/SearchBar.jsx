690288799ec96ff35263428f13c2a1505b504a89
visheshJS <visheshsharma_23me287@dtu.ac.in>
2025-03-15 13:35:04 +0530

    made responsive finally

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,17 +10,20 @@ const SearchBar = ({ onSearch }) => {
   };
 
   return (
-    <div className="flex items-center justify-center mt-8">
+    <div className="flex flex-col sm:flex-row items-center justify-center mt-8 space-y-4 sm:space-y-0 sm:space-x-4">
+      {/* Search Input */}
       <input
         type="text"
         placeholder="Search by book name or ISBN..."
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
-        className=" sm:min-w-sm sm:w-full max-w-md w-full bg-gray-100 px-4 py-2 border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-800"
+        className="w-full sm:w-auto sm:min-w-[500px] max-w-[300px] px-4 py-2 border-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-800"
       />
+      
+      {/* Search Button */}
       <button
         onClick={handleSearch}
-        className="ml-4 px-4 py-2 cursor-pointer transform duration-100 active:scale-90 bg-black text-white rounded-lg hover:bg-gray-800 transition"
+        className=" sm:w-auto px-4 py-2 cursor-pointer active:scale-90 transform duration-100 bg-black text-white rounded-lg hover:bg-gray-800 transition"
       >
         Search
       </button>
@@ -29,3 +32,4 @@ const SearchBar = ({ onSearch }) => {
 };
 
 export default SearchBar;
+
